8428bf12549b726285ca6773cd928aaf5d5ba403
DulakshiButhpitiya <dulakshibuthpitiya@gmail.com>
2025-06-30 21:40:10 +0530

    edit code

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -10,19 +10,15 @@ import uploads from '../utils/multer.js'; // Import multer configuration for ima
 
 const router = express.Router();
 
-// POST - Create product with image
+
 router.post("/", uploads.single("image"),createProduct);
 
-// GET all products
 router.get('/', getProducts);
 
-// GET one product
 router.get('/:id', getProductById);
 
-// PUT - Update product (optional image upload)
 router.put('/:id', uploads.single("image"), updateProduct); // make sure to use multer here too
 
-// DELETE
 router.delete('/:id', deleteProduct);
 
 export default router;
